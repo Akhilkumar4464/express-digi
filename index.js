477e563ea49a7eaa3fed8b26cb8856305b9e2b21
akhilkumar4464 <niteshsharma4464@gmail.com>
2025-07-20 19:15:34 +0530

    custom logger part will be done

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-import express from 'express';
+import logger from './logger.js';
+ import morgan from 'morgan';
+ import express from 'express';
+
 import 'dotenv/config'
 const app = express();
+const morganFormat = ":method :url :status :response-time ms";
 
 const PORT = process.env.PORT || 5000;
 
+
  app.use(express.json());
+ app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
   let teaData = []
    let nextId = 1;
